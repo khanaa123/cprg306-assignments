daa45a0e92fbf3c8b842716e95eec5e47364ad44
khanaa123 <khanaac@gmailcom>
2026-02-05 13:57:16 -0700

    week2

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,13 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Khankhuslen Chimeddorj</p>
+      <p>
+        GitHub:{' '}
+        <a href="https://github.com/khanaa123" target="_blank" rel="noopener noreferrer">
+          https://github.com/khanaa123
+        </a>
+      </p>
+    </div>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from './StudentInfo';
+
+export default function Week2Page() {
+  return (
+    <div>
+      <h1>Week 2</h1>
+      <StudentInfo />
+    </div>
+  );
+}
